dde0ed8a502d0a359426813a30e97b8f1378345e
LuisTrujillEspi <95892442+xultun@users.noreply.github.com>
2025-04-16 00:38:50 -0500

    Add files via upload

---
+++ users.js
@@ -0,0 +1,10 @@
+const users = [
+    {
+        fullname: "Usuario Prueba",
+        phone: "987654321",
+        email: "prueba@alertaride.com",
+        dni: "12345678",
+        dob: "2000-01-01",
+        password: "123456"
+    }
+];
